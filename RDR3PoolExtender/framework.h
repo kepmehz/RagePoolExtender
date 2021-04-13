8da7c57ddbc1d87cb1c94e926d8d3f1f6c7d77e9
kepmehz <kepmehzde@gmail.com>
2021-04-13 14:35:00 +0200

    removed useless comments

--- RDR3PoolExtender/framework.h
+++ RDR3PoolExtender/framework.h
@@ -1,7 +1,7 @@
 #pragma once
 
-#define WIN32_LEAN_AND_MEAN             // Selten verwendete Komponenten aus Windows-Headern ausschließen
-// Windows-Headerdateien
+#define WIN32_LEAN_AND_MEAN
+
 #include <windows.h>
 #include <cstdint>
 #include <vector>
